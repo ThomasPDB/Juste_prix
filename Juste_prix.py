081ac5c4f9fdbec926ecbd9735af6d14e8e65f6c
ThomasPDB <pereira.thomas@sfr.fr>
2021-09-26 15:51:34 +0200

    corr fr

--- Juste_prix.py
+++ Juste_prix.py
@@ -6,12 +6,12 @@ compteur = 100
 # prix à deviner
 juste_prix = randint(1, 1000)
 print("Bienvenue au Juste Prix !")
-print(f"vous avez {compteur} points")
+print(f"Vous avez {compteur} points")
 
 # fourchette prix
 x = max(1, juste_prix - randint(10, 100))
 y = min(1000, juste_prix + randint(10, 100))
-print(f"le prix est entre {x} et {y}")
+print(f"Le prix est entre {x} et {y}")
 
 
 def erreur(string):
@@ -28,24 +28,24 @@ lancement = True
 while lancement:
 
     # prix donné
-    prix_client = erreur(input("entrer chiffre : "))
+    prix_client = erreur(input("Votre Prix : "))
     if prix_client == False:
         print("Seul les nombres entier sont acceptés")
         continue
 
     # victoire
     if prix_client == juste_prix:
-        print("c'est gagné")
+        print("C'est gagné")
         lancement = False
 
     # c'est plus
     elif prix_client < juste_prix:
-        print("c'est plus")
+        print("C'est plus")
         compteur -= 1
 
     # c'est  moins
     elif prix_client > juste_prix:
-        print("c'est moins")
+        print("C'est moins")
         compteur -= 1
 
-print("fin du jeu")
+print("Fin du jeu")
