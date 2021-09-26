b623a4063b087ded00ce37a86819ac88c4e8290c
ThomasPDB <pereira.thomas@sfr.fr>
2021-09-26 15:49:21 +0200

    Juste_Prix_V2

--- Juste_prix.py
+++ Juste_prix.py
@@ -1,25 +1,37 @@
 from random import randint
 
+# compteur point
+compteur = 100
+
 # prix à deviner
 juste_prix = randint(1, 1000)
 print("Bienvenue au Juste Prix !")
+print(f"vous avez {compteur} points")
 
 # fourchette prix
 x = max(1, juste_prix - randint(10, 100))
 y = min(1000, juste_prix + randint(10, 100))
 print(f"le prix est entre {x} et {y}")
 
-# compteur point
-compteur = 100
+
+def erreur(string):
+    try:
+        return int(string)
+    except ValueError:
+        return False
+
 
 # lancement
 lancement = True
 
 # debut
 while lancement:
+
     # prix donné
-    print(f"vous avez {compteur} points")
-    prix_client = int(input("entrer chiffre : "))
+    prix_client = erreur(input("entrer chiffre : "))
+    if prix_client == False:
+        print("Seul les nombres entier sont acceptés")
+        continue
 
     # victoire
     if prix_client == juste_prix:
